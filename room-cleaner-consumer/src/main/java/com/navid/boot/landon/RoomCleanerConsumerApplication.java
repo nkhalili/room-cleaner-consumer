a5270b83d5e3329fb7e2ac9c13de10bf95b7351f
Navid Khalili <nkhalili@live.com>
2020-08-03 13:29:00 -0400

    init rabit project

---
+++ room-cleaner-consumer/src/main/java/com/navid/boot/landon/RoomCleanerConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.navid.boot.landon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomCleanerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomCleanerConsumerApplication.class, args);
+	}
+
+}
